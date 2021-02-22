6d232ab1222cee3e3c14676e481702746978d073
shigasy <kxx555u.u@gmail.com>
2021-02-23 02:24:42 +0900

    added install.sh

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 現在はmacのみで動くようになっているが、その他の環境でも動かせるようにしたい
 
 ```
-curl -LSfs https://raw.githubusercontent.com/shigasy/dotfiles/main/install | sh
+curl -LSfs https://raw.githubusercontent.com/shigasy/dotfiles/main/install.sh | bash -s -d
 ```
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# このスクリプトを実行している位置にカレントディレクトリが来る
+
+deploy() {
+
+  echo "deploy"
+
+}
+
+initialize() {
+
+  echo "initialize"
+
+}
+
+# -deploy or -d
+if [ "$1" = "deploy" -o "$1" = "d" ]; then
+    deploy
+# -deploy or -i
+elif [ "$1" = "init" -o "$1" = "i" ; then
+    initalize
+fi
+
+# DOTPATHが空文字の場合、$HOMEが入る。DOTPATHに$HOMEを保存しない
+DOTPATH=${DOTPATH:-$HOME/.dotfiles}
+REPOSITORY=shigasy/dotfiles
+
+echo $DOTPATH
+echo "install...."
